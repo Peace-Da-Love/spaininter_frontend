5969fc8a7b75c7dbbe643e87c3b9a954bc9c0f13
Stepan <stepan663663@gmail.com>
2024-05-21 23:18:02 +0300

    changed: main widget

--- src/widgets/main/view.tsx
+++ src/widgets/main/view.tsx
@@ -1,6 +1,5 @@
 import { FC, ReactNode } from 'react';
 import { SiteMenu } from '@/src/widgets/site-menu';
-import { RequestMenu } from '@/src/widgets/request-menu';
 
 type Props = {
 	children: ReactNode;
@@ -14,7 +13,6 @@ export const Main: FC<Props> = ({ children }) => {
 			<SiteMenu
 				className={'fixed bottom-2.5 right-2.5 z-50 inline-flex md:hidden'}
 			/>
-			<RequestMenu className={'fixed bottom-2.5  z-50'} />
 		</main>
 	);
 };
