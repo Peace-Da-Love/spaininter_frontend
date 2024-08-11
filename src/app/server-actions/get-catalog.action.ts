387377dc74fb1b8fd4270bb2083a7ec13ce9027b
Stepan <stepan663663@gmail.com>
2024-08-11 21:41:46 +0300

    added: fetch' api endpoint

---
+++ src/app/server-actions/get-catalog.action.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import { $fetchS } from '../server-api';
+import { Property } from '@/src/shared/types';
+
+type Params = {
+	locale: string;
+};
+
+export async function getCatalog(
+	params: Params
+): Promise<Property[] | undefined> {
+	const response = await $fetchS(
+		`properties`,
+		{
+			headers: {
+				'Accept-Language': params.locale
+			}
+		},
+		'catalog'
+	);
+	if (!response.ok) return undefined;
+	const data = (await response.json()) as Property[];
+
+	return data;
+}
