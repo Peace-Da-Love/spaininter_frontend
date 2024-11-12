1695378e748864f97b6254a42d5585374400f24d
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-11-12 20:17:03 +0300

    UPDATE: Card menu expanded was changed

--- src/entities/city-card/view.tsx
+++ src/entities/city-card/view.tsx
@@ -2,7 +2,7 @@
 import { FC, useState } from 'react';
 import { TTag } from '@/src/shared/types';
 import Image from 'next/image';
-import * as Collapsible from '@radix-ui/react-collapsible';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { twMerge } from 'tailwind-merge';
 import ExpandArrow from '@/src/app/icons/ic_expand_arrow.svg';
 import Chat from '@/src/app/icons/ic_chat.svg';
@@ -32,88 +32,121 @@ const CityCard: FC<Props> = ({
 
 	const Tag = tag || 'div';
 	return (
-		<Tag
-			className={twMerge(
-				`rounded-[25px_25px_25px_25px/35px_35px_35px_35px] overflow-hidden ${className}`
-			)}
-		>
-			<div className='w-full relative'>
-				{screenSize > 950 && (
-					<div className='flex items-center gap-x-[10px] absolute bottom-[16px] left-[20px]'>
-						<Chat />
-						<h3 className='text-white font-sans font-medium text-sm'>
-							Форум/Чат
-						</h3>
-					</div>
-				)}
-				<Image
-					src={imageUrl}
-					alt={title}
-					width={155}
-					height={90}
-					className='w-full h-auto max-h-[180px] object-cover'
-				/>
-			</div>
-			<div className='bg-[#E9E9F0]'>
-				{screenSize > 950 ? (
-					<div className='pl-[21px] pb-[25px] pr-[15px] pt-[15px]'>
-						<div
-							className={`flex w-full justify-between items-center mb-[15px] $`}
-						>
-							<h2 className='font-sans text-xl text-[#192E51] font-bold'>
-								{title}
-							</h2>
+		<>
+			{screenSize > 950 ? (
+				<Tag
+					className={twMerge(
+						`rounded-[25px_25px_25px_25px/35px_35px_35px_35px] overflow-hidden ${className}`
+					)}
+				>
+					<div className='w-full relative'>
+						<div className='flex items-center gap-x-[10px] absolute bottom-[16px] left-[20px]'>
+							<Chat />
+							<h3 className='text-white font-sans font-medium text-sm'>
+								Форум/Чат
+							</h3>
 						</div>
-						<nav className='flex flex-wrap gap-y-[10px] gap-x-[7px]'>
-							{links.map(({ link, text }) => {
-								return (
-									<Link
-										key={link}
-										href={link}
-										className='text-xs font-sans text-center text-white bg-[#607698] p-[7px] rounded-[50px] hover:bg-[#7a96c0] duration-200 active:bg-[#7a96c0] min-w-[100px] flex-[1_1_calc(33.33%-7px)]'
-									>
-										{text}
-									</Link>
-								);
-							})}
-						</nav>
+
+						<Image
+							src={imageUrl}
+							alt={title}
+							width={155}
+							height={90}
+							className='w-full h-auto max-h-[180px] object-cover'
+						/>
 					</div>
-				) : (
-					<Collapsible.Root
-						open={isLinksOpen}
-						onOpenChange={setIsLinksOpen}
-						className='p-[10px] pb-[15px]'
-					>
-						<Collapsible.Trigger
-							className={`flex w-full justify-between items-center ${isLinksOpen &&
-								'mb-[7px]'}`}
-						>
-							<h2 className='font-sans text-xs'>{title}</h2>
+					<div className='bg-[#E9E9F0]'>
+						<div className='pl-[21px] pb-[25px] pr-[15px] pt-[15px]'>
 							<div
-								className={`duration-200 ${
-									isLinksOpen ? '-rotate-180' : 'rotate-0'
+								className={`flex w-full justify-between items-center mb-[15px] $`}
+							>
+								<h2 className='font-sans text-xl text-[#192E51] font-bold'>
+									{title}
+								</h2>
+							</div>
+							<nav className='flex flex-wrap gap-y-[10px] gap-x-[7px]'>
+								{links.map(({ link, text }) => {
+									return (
+										<Link
+											key={link}
+											href={link}
+											className='text-xs font-sans text-center text-white bg-[#607698] p-[7px] rounded-[50px] hover:bg-[#7a96c0] duration-200 active:bg-[#7a96c0] min-w-[100px] flex-[1_1_calc(33.33%-7px)]'
+										>
+											{text}
+										</Link>
+									);
+								})}
+							</nav>
+						</div>
+					</div>
+				</Tag>
+			) : (
+				<Tag
+					className={twMerge(
+						` ${
+							isLinksOpen
+								? 'rounded-[25px_25px_0_0/35px_35px_0_0] relative z-[2]'
+								: 'rounded-[25px_25px_25px_25px/35px_35px_35px_35px]'
+						} overflow-hidden ${className}`
+					)}
+				>
+					<DropdownMenu.Root open={isLinksOpen} onOpenChange={setIsLinksOpen}>
+						<DropdownMenu.Trigger className='w-full focus:outline-none'>
+							<div className='w-full relative'>
+								<Image
+									src={imageUrl}
+									alt={title}
+									width={155}
+									height={90}
+									className='w-full h-auto max-h-[180px] object-cover'
+								/>
+							</div>
+							<div
+								className={`duration-100 ${
+									isLinksOpen ? 'bg-[#d8d8df]' : 'bg-[#E9E9F0]'
 								}`}
 							>
-								<ExpandArrow />
-							</div>
-						</Collapsible.Trigger>
-						<Collapsible.Content className='flex flex-wrap gap-y-[10px]'>
-							{links.map(({ link, text }) => {
-								return (
-									<Link
-										key={link}
-										href={link}
-										className='text-xs font-sans text-center text-white bg-[#607698] p-[7px] rounded-[50px] hover:bg-[#7a96c0] duration-200 active:bg-[#7a96c0] w-full '
+								<div
+									className={`p-[10px] ${
+										isLinksOpen ? 'pb-[10px]' : 'pb-[15px]'
+									} `}
+								>
+									<div className='flex w-full justify-between items-center'>
+										<h2 className='font-sans text-xs'>{title}</h2>
+										<div
+											className={`duration-200 ${
+												isLinksOpen ? '-rotate-180' : 'rotate-0'
+											}`}
+										>
+											<ExpandArrow />
+										</div>
+									</div>
+									<DropdownMenu.Content
+										className={`w-[var(--radix-popper-anchor-width)] duration-100 ${
+											isLinksOpen ? 'bg-[#d8d8df]' : 'bg-[#E9E9F0]'
+										} rounded-[0px_0px_25px_25px/0px_0px_35px_35px] mx-auto box-border`}
 									>
-										{text}
-									</Link>
-								);
-							})}
-						</Collapsible.Content>
-					</Collapsible.Root>
-				)}
-			</div>
-		</Tag>
+										<div className='w-full p-[10px] pb-[15px] pt-0 flex flex-wrap gap-y-[10px] '>
+											{links.map(({ link, text }) => {
+												return (
+													<Link
+														key={link}
+														href={link}
+														className='text-xs block font-sans text-center text-white bg-[#607698] p-[7px] rounded-[50px] hover:bg-[#7a96c0] duration-200 active:bg-[#7a96c0] w-full focus:outline-none '
+													>
+														{text}
+													</Link>
+												);
+											})}
+										</div>
+									</DropdownMenu.Content>
+								</div>
+							</div>
+						</DropdownMenu.Trigger>
+					</DropdownMenu.Root>
+				</Tag>
+			)}
+		</>
 	);
 };
 
