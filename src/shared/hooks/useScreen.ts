8a0508452485437ef2445659679a66198d084c95
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-11-12 18:32:18 +0300

    FIX: Fidxed deploy issue (window is not defined)

--- src/shared/hooks/useScreen.ts
+++ src/shared/hooks/useScreen.ts
@@ -1,9 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 
 export const useScreen = (): number => {
-	const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth)
-	window.addEventListener('resize', () => {
-		setScreenWidth(window.innerWidth)
-	})
-	return screenWidth
-}
+	const [screenWidth, setScreenWidth] = useState<number>(0);
+	useEffect(() => {
+		if (typeof window !== undefined) {
+			setScreenWidth(window.innerWidth);
+			window.addEventListener('resize', () => {
+				setScreenWidth(window.innerWidth);
+			});
+		}
+	}, []);
+
+	return screenWidth;
+};
