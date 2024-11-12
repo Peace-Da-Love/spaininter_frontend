cba2bac572d8d2485aea704a86969f290996b4f1
Dzhambulat Kagermanov <dzhambulat.kagermanov@gmail.com>
2024-11-12 18:22:45 +0300

    UPDATE: Added adaptive for cards

--- src/entities/city-card/view.tsx
+++ src/entities/city-card/view.tsx
@@ -5,7 +5,9 @@ import Image from 'next/image';
 import * as Collapsible from '@radix-ui/react-collapsible';
 import { twMerge } from 'tailwind-merge';
 import ExpandArrow from '@/src/app/icons/ic_expand_arrow.svg';
+import Chat from '@/src/app/icons/ic_chat.svg';
 import Link from 'next/link';
+import { useScreen } from '@/src/shared/hooks';
 
 interface Props {
 	tag?: TTag;
@@ -26,6 +28,7 @@ const CityCard: FC<Props> = ({
 	cardInfo: { id, imageUrl, links, title }
 }) => {
 	const [isLinksOpen, setIsLinksOpen] = useState<boolean>(false);
+	const screenSize = useScreen();
 
 	const Tag = tag || 'div';
 	return (
@@ -34,45 +37,81 @@ const CityCard: FC<Props> = ({
 				`rounded-[25px_25px_25px_25px/35px_35px_35px_35px] overflow-hidden ${className}`
 			)}
 		>
-			<Image
-				src={imageUrl}
-				alt={title}
-				width={155}
-				height={90}
-				className='w-full h-auto max-h-[180px] object-cover'
-			/>
+			<div className='w-full relative'>
+				{screenSize > 950 && (
+					<div className='flex items-center gap-x-[10px] absolute bottom-[16px] left-[20px]'>
+						<Chat />
+						<h3 className='text-white font-sans font-medium text-sm'>
+							Форум/Чат
+						</h3>
+					</div>
+				)}
+				<Image
+					src={imageUrl}
+					alt={title}
+					width={155}
+					height={90}
+					className='w-full h-auto max-h-[180px] object-cover'
+				/>
+			</div>
 			<div className='bg-[#E9E9F0]'>
-				<Collapsible.Root
-					open={isLinksOpen}
-					onOpenChange={setIsLinksOpen}
-					className=' p-[10px] pb-[15px]'
-				>
-					<Collapsible.Trigger
-						className={`flex w-full justify-between items-center ${isLinksOpen &&
-							'mb-[7px]'}`}
-					>
-						<h2 className='font-sans text-xs'>{title}</h2>
+				{screenSize > 950 ? (
+					<div className='pl-[21px] pb-[25px] pr-[15px] pt-[15px]'>
 						<div
-							className={`duration-200 ${
-								isLinksOpen ? '-rotate-180' : 'rotate-0'
-							}`}
+							className={`flex w-full justify-between items-center mb-[15px] $`}
 						>
-							<ExpandArrow />
+							<h2 className='font-sans text-xl text-[#192E51] font-bold'>
+								{title}
+							</h2>
 						</div>
-					</Collapsible.Trigger>
-					<Collapsible.Content className='flex flex-wrap gap-y-[10px]'>
-						{links.map(({ link, text }) => {
-							return (
-								<Link
-									href={link}
-									className='text-xs font-sans text-center text-white bg-[#607698] p-[7px] rounded-[50px] hover:bg-[#7a96c0] duration-200 active:bg-[#7a96c0] w-full '
-								>
-									{text}
-								</Link>
-							);
-						})}
-					</Collapsible.Content>
-				</Collapsible.Root>
+						<nav className='flex flex-wrap gap-y-[10px] gap-x-[7px]'>
+							{links.map(({ link, text }) => {
+								return (
+									<Link
+										key={link}
+										href={link}
+										className='text-xs font-sans text-center text-white bg-[#607698] p-[7px] rounded-[50px] hover:bg-[#7a96c0] duration-200 active:bg-[#7a96c0] min-w-[100px] flex-[1_1_calc(33.33%-7px)]'
+									>
+										{text}
+									</Link>
+								);
+							})}
+						</nav>
+					</div>
+				) : (
+					<Collapsible.Root
+						open={isLinksOpen}
+						onOpenChange={setIsLinksOpen}
+						className='p-[10px] pb-[15px]'
+					>
+						<Collapsible.Trigger
+							className={`flex w-full justify-between items-center ${isLinksOpen &&
+								'mb-[7px]'}`}
+						>
+							<h2 className='font-sans text-xs'>{title}</h2>
+							<div
+								className={`duration-200 ${
+									isLinksOpen ? '-rotate-180' : 'rotate-0'
+								}`}
+							>
+								<ExpandArrow />
+							</div>
+						</Collapsible.Trigger>
+						<Collapsible.Content className='flex flex-wrap gap-y-[10px]'>
+							{links.map(({ link, text }) => {
+								return (
+									<Link
+										key={link}
+										href={link}
+										className='text-xs font-sans text-center text-white bg-[#607698] p-[7px] rounded-[50px] hover:bg-[#7a96c0] duration-200 active:bg-[#7a96c0] w-full '
+									>
+										{text}
+									</Link>
+								);
+							})}
+						</Collapsible.Content>
+					</Collapsible.Root>
+				)}
 			</div>
 		</Tag>
 	);
