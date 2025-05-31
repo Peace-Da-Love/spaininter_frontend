15d31090fbd8e1c0f7e7c5ab091d958108d78891
aidaraitymkulov <aitymkulovich@gmail.com>
2025-06-01 05:27:03 +0600

    metadata for telegram-channels page

--- app/[locale]/catalog-telegram/page.tsx
+++ app/[locale]/catalog-telegram/page.tsx
@@ -1,15 +1,50 @@
 import { CatalogTelegram } from '@/src/screens/catalog-telegram';
+import { locales } from '@/src/shared/configs';
 import { Metadata } from 'next';
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 import { FC } from 'react';
 
-export const metadata: Metadata = {
-	title: 'Каталог Telegram каналов и чатов Испания',
-	description:
-		'Telegram каналы Испании Мадрид, Барселона, Валенсия, Севилья, Сарагоса, Малага, Мурсия'
+type Props = {
+	params: { locale: string };
 };
 
-interface Props {}
-const Page: FC<Props> = ({}) => {
+const SITE_URL = process.env.SITE_URL;
+
+export async function generateMetadata({
+	params: { locale }
+}: Omit<Props, 'children'>): Promise<Metadata> {
+	const t = await getTranslations({
+		locale,
+		namespace: 'MetaData.CatalogTelegramPage'
+	});
+
+	const hrefLangs: Record<string, string> = locales.reduce((acc, loc) => {
+		acc[loc] = `${SITE_URL}/${loc}/catalog-telegram`;
+		return acc;
+	}, {} as Record<string, string>);
+
+	return {
+		title: t('title'),
+		description: t('description'),
+		alternates: {
+			languages: {
+				'x-default': hrefLangs['en'],
+				...hrefLangs
+			},
+			canonical: hrefLangs['en']
+		},
+		openGraph: {
+			title: t('title'),
+			description: t('description'),
+			url: hrefLangs[locale],
+			locale,
+			type: 'website'
+		}
+	};
+}
+
+const Page: FC<Props> = ({ params: { locale } }) => {
+	unstable_setRequestLocale(locale);
 	return <CatalogTelegram />;
 };
 
