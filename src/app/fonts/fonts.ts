3ded520183425389fc783765f14a66e8e34b0b33
Валерий <valera210597@gmail.com>
2025-10-22 16:21:23 +0300

    feat(fonts): expand Montserrat font configuration to include additional subsets and weights, and set display to swap

--- src/app/fonts/fonts.ts
+++ src/app/fonts/fonts.ts
@@ -1,6 +1,8 @@
 import { Montserrat } from 'next/font/google';
 
 export const montserrat = Montserrat({
-	subsets: ['latin'],
+	subsets: ['latin', 'latin-ext', 'cyrillic', 'cyrillic-ext'],
+	weight: ['300', '400', '500', '600', '700'],
+	display: 'swap',
 	variable: '--montserrat-font'
 });
