b4736b4b980067c74dd2f507bf7cf8947c7323f1
Валерий <valera210597@gmail.com>
2026-04-25 22:49:15 +0300

    fix(menu): point CitiesButton to Telegram list

--- src/features/cities-button/view.tsx
+++ src/features/cities-button/view.tsx
@@ -1,33 +1,19 @@
 'use client';
-import { ChannelLink } from '@/src/shared/utils';
 import IcCities from '@/src/app/icons/ic_cities.svg';
-import IcNewspaper from '@/src/app/icons/ic_newspaper.svg';
 import { Button } from '@/src/shared/components/ui';
-import { usePathname } from 'next/navigation';
-import { locales } from '@/src/shared/configs';
 import { forwardRef } from 'react';
 
 export const CitiesButton = forwardRef<HTMLButtonElement, {}>((props, ref) => {
-	const path = usePathname();
-	const locale = path.split('/').filter(Boolean)[0] || 'en';
-	const thisPageIsCatalogTelegram: string | undefined = locales.find(locale => {
-		return `/${locale}/catalog-telegram` === path || `/${locale}/tma/catalog-telegram` === path;
-	});
-	
 	return (
 		<Button variant={'menu'} asChild>
-			<ChannelLink
-				locale={locale}
-				href={
-					thisPageIsCatalogTelegram !== undefined ? '/news' : '/catalog-telegram'
-				}
+			<a
+				href="https://t.me/addlist/zixZWkXQiKY2MmVi"
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label="Cities"
 			>
-				{thisPageIsCatalogTelegram !== undefined ? (
-					<IcNewspaper />
-				) : (
-					<IcCities />
-				)}
-			</ChannelLink>
+				<IcCities />
+			</a>
 		</Button>
 	);
 });
